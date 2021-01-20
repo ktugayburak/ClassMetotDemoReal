9ffef14565adbc73ea2f1b054ed24be886d82396
Burak Köprülü <ktugayburak@gmail.com>
2021-01-20 19:45:43 +0300

    Add project files.

---
+++ ClassMetotDemoReal/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemoReal
+{
+    class Customer
+    {
+        public int Id { get; set; }
+
+        public string AdSoyad { get; set; }
+
+        public int Age { get; set; }
+
+        public string Sex { get; set; }
+
+        public string Job { get; set; }
+
+    }
+}
